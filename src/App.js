368fdfad0fbe7e96f619bef39acfbf062f6c25e3
tribenetmedia <balazs@tribenetmedia.com>
2023-12-17 21:45:39 +0100

    origin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Model } from "survey-core";
+import { Survey } from "survey-react-ui";
+import "survey-core/defaultV2.min.css";
+import "./index.css";
+import { json } from "./json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function SurveyComponent() {
+    const survey = new Model(json);
+    survey.onComplete.add((sender, options) => {
+        console.log(JSON.stringify(sender.data, null, 3));
+    });
+    return (<Survey model={survey} />);
 }
 
-export default App;
+export default SurveyComponent;
